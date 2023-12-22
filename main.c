ed8a9c1f5c80b068d6bf869fa4ea7aad3d4eb5da
leejaeyoung1023 <116942871+leejaeyoung1023@users.noreply.github.com>
2023-12-22 14:16:38 +0000

    231222

--- main.c
+++ main.c
@@ -3,40 +3,46 @@
 #include <time.h>
 
 int main(void){
-    int i;
-    int num1,num2;
-    int cnt=0,win=0,lose=0,draw=0;
+    int i1,i2;
+    int com[3],num[3];
+    int strike=0,ball=0,cnt=0;
     
     srand(time(NULL));
 
-    while (num1!=4)
-    {
-        printf("바위는 1, 가위는 2, 보는 3 결과확인은 4\n");
-        printf("선택: ");
-        
-        scanf("%d", &num1);
-        num2=rand()%3+1;
-
-        if(num1==1)printf("당신은 바위를 선택  ");
-        if(num1==2)printf("당신은 가위를 선택  ");
-        if(num1==3)printf("당신은 보를 선택  ");
     
-        if(num2==1)printf("컴퓨터는 바위를 선택\n ");
-        if(num2==2)printf("컴퓨터는 가위를 선택\n ");
-        if(num2==3)printf("컴퓨터는 보를 선택\n ");
-
-        if((num1==1&&num2==2)||(num1==2&&num2==3)||(num1==3&&num2==1)){ printf("이겼습니다\n\n"); win++; }
-        else if(num1!=num2) 
-        { printf("졌습니다\n\n"); lose++; }
-
-        if(num1==num2){
-            printf("비겼습니다\n\n");
-            draw++;    
+    
+    for(i1=0;i1<3;i1++){
+        com[i1]=rand()%9;
+        for(i2=0;i2<i1;i2++){
+            if(com[i1]==com[i2])i1--;
+        }        
+    }
+    printf("%d", com[0]);    
+    printf("%d", com[1]);
+    printf("%d", com[2]);
+    printf("start game\n\n");
+    
+    while(strike!=3){
+        cnt++; strike=ball=0;
+        printf("3개의 숫자 선택: ");
+        
+        for(i1=0;i1<3;i1++){
+            scanf("%d", &num[i1]);
         }
-        cnt++;
+        
+        for(i1=0;i1<3;i1++){
+            if(com[i1]==num[i1])strike++; 
+            else 
+            for(i2=0;i2<3;i2++){
+                if(com[i1]==num[i2])ball++;
+            }
+        }
+        
+        printf("%d번째 도전 결과: ", cnt);
+        printf("%dstrike, %dball \n\n", strike, ball);
     }
     
-    printf("게임의 결과 : %d판 %d승, %d무, %d패 \n\n",cnt, win, draw, lose);    
+    printf("game set\n\n");
 
     return 0;
 
